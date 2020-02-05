a35d819d51bb5732aa2fe42804ce1df3a223d3e2
saidaguilar619 <58634200+saidaguilar619@users.noreply.github.com>
2020-02-04 17:04:34 -0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,38 +1,39 @@
-const timeArr = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", 
+const timeArr = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm",  //setting array with time values
                  "1pm", "2pm", "3pm", "4pm", "5pm","6pm","7pm","8pm"];
 let currentTime = moment().format("HH");
-timeStart = 6;
-
-let blockTimeStart;
 
 function setCalander(){
+    let timeStart = 6; 
+    $("#currentDay").text(moment()); //displays current time
     for (let i = 0; i < timeArr.length;i++ ){
-        let newHourH1 = $("<h1>");
+        //declaring important variables
         let displayHour = timeArr[i];
+        
         let blockTimeStart = timeStart;
         let eventBoxNum = "eventBox" + i;
         let eventBN = ".eventBox" + i;
 
+        //creating new elements
+        let newHourH1 = $("<h1>"); 
+        newHourH1.text(displayHour);
+
         let saveButton = $("<img>").attr("src", "https://cdn3.iconfinder.com/data/icons/ui-essential-elements-buttons/110/Save-512.png");
         saveButton.attr("width", "50px");
         saveButton.attr("height", "50px");
-        
-
         saveButton.css("margin-left", "11%");
         saveButton.css("margin-top", "3%");
-
+        
         let newRowEl = $("<div>").addClass("row");
         $(".container").append(newRowEl);
-        // let newP = $("<p>").addClass("eventInfo");
-        // newP.val(localStorage.getItem(eventBN));
 
         let newHourEl = $("<div>").addClass("hour col-2");
+
         let newEventBlock = $("<input>").addClass("eventBlock col-8");
         newEventBlock.val(localStorage.getItem(eventBN));
-        console.log(localStorage.eventBN);
         newEventBlock.attr("id", i);
         newEventBlock.addClass(eventBoxNum);
         
+        //check if time block is current future or past
         if(currentTime == blockTimeStart )
         {
             newEventBlock.addClass("present");
@@ -46,24 +47,22 @@ function setCalander(){
             newEventBlock.addClass("past");
         }
 
-        let newSaveBtn = $("<button>").addClass("saveButton col-2");
+        let newSaveBtn = $("<button>").addClass("saveButton col-2"); //creates save button
         newSaveBtn.attr("id", i);
         newSaveBtn.append(saveButton);
 
         newRowEl.append(newHourEl, newEventBlock, newSaveBtn);
         newHourEl.append(newHourH1);
-        newHourH1.text(displayHour);
+
         timeStart++;
     }
 }
 setCalander();  
 
+//saves info in local storage when save button is clicked
 $(".saveButton").on("click", function(event){
-    
-    let ebox = this.id;
-    let boxNum = ".eventBox" + ebox; 
+    let boxNum = ".eventBox" + this.id; 
     let discription = $(boxNum).val();
-    
-    console.log(discription);
+ 
     localStorage.setItem(boxNum, discription);
 })
